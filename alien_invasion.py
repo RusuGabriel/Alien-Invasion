74f9ebb27931d8ce4814a9c882bd0148751c7921
Rusu Gabriel <rusugabriel13@gmail.com>
2019-09-12 09:01:44 +0300

    Final Version of The Game

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from pygame.sprite import Group
 
 from settings import Settings
+from scoreboard import Scoreboard
 from button import Button
 from ship import Ship
 import game_functions as gf
@@ -23,7 +24,7 @@ def run_game():
 
     # Create an instance to store game statistics.
     stats = GameStats(ai_settings)
-
+    sb = Scoreboard(ai_settings, screen, stats)
     # create a fleet of aliens.
     gf.create_fleet(ai_settings, screen, ship, aliens)
     pygame.display.set_caption("Alien Invasion")
@@ -32,13 +33,14 @@ def run_game():
     play_button = Button(ai_settings, screen, "Play")
     # Start the main loop for the game.
     while True:
-        gf.check_events(ai_settings, screen, stats,
+        gf.check_events(ai_settings, screen, stats,sb,
                         play_button, ship, aliens, bullets)
         if stats.game_active:
             ship.update()
-            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
-            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
-        gf.update_screen(ai_settings, screen, stats, ship,
+            gf.update_bullets(ai_settings, screen, stats,
+                              sb, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, screen, stats, sb, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, stats, sb, ship,
                          aliens, bullets, play_button)
 
 
--- settings.py
+++ settings.py
@@ -7,20 +7,36 @@ class Settings():
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
-        
+
         # Ship settings
         self.ships_limit = 3
-        self.ship_speed_factor = 1.5
-        
+
         # Bullet settings
-        self.bullet_speed_factor = 3
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
         self.bullets_allowed = 3
-        
-        #Alien settings
-        self.alien_speed_factor = 1
+        self.score_scale = 1.5
+
+        # How quickly the game speeds up
+        self.speedup_scale = 1.1
         self.fleet_drop_speed = 10
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """Initialize settings that change troughout the game."""
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 3
+        self.alien_speed_factor = 1
         # fleet_ direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
+        self.alien_points = 50
+        
+    
+    def increase_speed(self):
+        """Increase speedsettings."""
+        self.ship_speed_factor*= self.speedup_scale
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale
+        self.alien_points = int(self.alien_points * self.score_scale)
